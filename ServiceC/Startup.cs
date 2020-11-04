2a98a847ea74819e1be997d5acf971840508389e
ovidiu buligan <ovidiu.buligan@visma.com>
2020-11-04 21:25:52 +0200

    add serviceB and C autoinstrumentation

--- ServiceC/Startup.cs
+++ ServiceC/Startup.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OpenTelemetry.Trace;
 
 namespace ServiceC
 {
@@ -26,6 +28,38 @@ namespace ServiceC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddOpenTelemetryTracing(
+                        (serviceProvider, builder) =>
+                        {
+                            var serviceName = "C";
+                            var resource = OpenTelemetry.Resources.Resources.CreateServiceResource(serviceName);
+                            builder
+                                 .SetResource(resource)
+                                 .AddSource(serviceName)
+                                 .SetSampler(new AlwaysOnSampler())
+                                 .AddHttpClientInstrumentation()
+                                 .AddAspNetCoreInstrumentation(config => {
+                                     config.Enrich = (activity, str, obj) =>
+                                     {
+                                         if (str == "OnStopActivity" && obj is HttpResponse httpResponse)
+                                         {
+                                             if (httpResponse.StatusCode >= 500)
+                                             {
+                                                             // in order to show as an error in jaeger it needs error=true tag
+                                                             activity.SetTag("error", true);
+                                             }
+                                         }
+                                     };
+                                     config.RecordException = true;
+                                 })
+                                 .AddJaegerExporter((o) =>
+                                 {
+                                     o.AgentHost = "localhost";
+                                     o.AgentPort = 6835;
+                                     o.ServiceName = serviceName;
+                                 });
+
+                        });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
